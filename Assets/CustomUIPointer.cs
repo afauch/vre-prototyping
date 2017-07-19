fc4bc8a632bc9ee9d0d7f86bfc9c0cd64fe70e2e
Aaron Faucher <aaron.faucher@gmail.com>
2017-07-18 23:33:55 -0700

    UI Laser

---
+++ Assets/CustomUIPointer.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK;
+
+public class CustomUIPointer : MonoBehaviour {
+
+    public VRTK_ControllerEvents _vrtkControllerEvents;
+    public GameObject _origin;
+    public LayerMask _layerMask;
+    public float _maxDistance = 100.0f;
+
+    [Header ("Pointer Appearance")]
+    private LineRenderer _lr;
+    public Material _lineMaterial;
+    public Color _lineColor;
+    public float _lineWidth = 0.01f;
+    private RaycastHit _hitInfo;
+
+	// Use this for initialization
+	void Start () {
+
+        // Subscribe to events
+        _vrtkControllerEvents = _vrtkControllerEvents ?? this.gameObject.GetComponent<VRTK_ControllerEvents>();
+        _vrtkControllerEvents.TriggerClicked += new ControllerInteractionEventHandler(DoTriggerClicked);
+
+        // Setup LineRenderer
+        InitLineRenderer();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        // Always be raycasting
+        if (Physics.Raycast(_origin.transform.position, _origin.transform.forward, out _hitInfo, _maxDistance, _layerMask.value))
+        {
+            DoHitUI(_hitInfo);
+        } else
+        {
+            RenderLine(_lr, false);
+        }
+		
+	}
+
+
+    void InitLineRenderer()
+    {
+
+        _lr = this.gameObject.AddComponent<LineRenderer>();
+        Debug.Log(_lr);
+        _lr.enabled = false;
+        _lr.material = _lineMaterial;
+        _lr.startColor = _lineColor;
+        _lr.endColor = _lineColor;
+        _lr.widthMultiplier = _lineWidth;
+        _lr.startWidth = _lineWidth;
+        _lr.endWidth = _lineWidth;
+
+    
+    }
+
+    /// <summary>
+    /// Custom Trigger Click Handling
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
+    void DoTriggerClicked(object sender, ControllerInteractionEventArgs e)
+    {
+        Debug.Log("Trigger Clicked");
+    }
+
+    void DoHitUI(RaycastHit hit)
+    {
+        Debug.Log("Intersected 3D UI: " + hit.collider.gameObject.name);
+        RenderLine(_lr, true);
+ 
+    }
+
+    void RenderLine(LineRenderer lr, bool toggle)
+    {
+        Debug.Log("RenderLine called");
+        lr.enabled = toggle;
+
+        if (lr.enabled)
+        {
+
+            Vector3[] linePositions = new Vector3[]
+            {
+                   _origin.transform.position,
+                   _hitInfo.point
+            };
+            lr.SetPositions(linePositions);
+
+        }
+
+    }
+
+
+}
