2c6476833987147b4645e8c7c8ad1dd3576aeafa
Aaron Faucher <aaron.faucher@gmail.com>
2017-07-18 20:53:19 -0700

    Working swipe detector

---
+++ Assets/EntityPalette.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EntityPalette : MonoBehaviour {
+
+    public GameObject _page1;
+    public GameObject _page2;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void ShowPage1()
+    {
+        _page2.SetActive(false);
+        _page1.SetActive(true);
+    }
+
+    public void ShowPage2()
+    {
+        _page1.SetActive(false);
+        _page2.SetActive(true);
+    }
+
+}
