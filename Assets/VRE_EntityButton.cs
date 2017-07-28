cb24f8a4f2575d08aa22e03a93d4f9c06d4b6025
Aaron Faucher <aaron.faucher@gmail.com>
2017-07-28 09:44:17 -0700

    Functional Object Instantiation

---
+++ Assets/VRE_EntityButton.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK;
+
+public class VRE_EntityButton : MonoBehaviour {
+
+    public GameObject _entityToSpawn;
+    // public VRTK_ControllerEvents _vrtkControllerEvents;
+
+
+    private void Start()
+    {
+
+
+    }
+
+    public void DoSelect()
+    {
+
+        SpawnInstance();
+
+    }
+
+    private void SpawnInstance()
+    {
+        GameObject instance = GameObject.Instantiate(_entityToSpawn);
+
+        // get controller attach point
+        // Rigidbody attachPoint = _vrtkControllerEvents.gameObject.GetComponent<VRTK_InteractGrab>().controllerAttachPoint;
+        Rigidbody attachPoint = VRE_Globals._instance._rightHandControllerEvents.GetComponent<VRTK_InteractGrab>().controllerAttachPoint;
+
+        instance.transform.position = attachPoint.gameObject.transform.position;
+
+
+    }
+
+
+}
